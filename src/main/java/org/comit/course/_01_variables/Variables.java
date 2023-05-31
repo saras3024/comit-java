44af1e55455f9a6a67ed5716abc969cfa658f932
saras3024 <saraswathi1404@gmail.com>
2023-05-30 20:03:56 -0600

    first message

---
+++ src/main/java/org/comit/course/_01_variables/Variables.java
@@ -0,0 +1,13 @@
+package org.comit.course._01_variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int num = 5;
+		
+		System.out.println("The value is :" + num);
+
+	}
+
+}
